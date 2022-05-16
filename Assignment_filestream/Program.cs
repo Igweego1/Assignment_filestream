fe093f63d4efed276420ea3a644515998e4dfd3c
Obiora Igwilo <igwiloobiora@outlook.com>
2022-05-16 15:41:08 +0100

    Add project files.

---
+++ Assignment_filestream/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Assignment_filestream
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var studentinfo = Studentdata.GetStudentdatas();
+
+
+
+
+            string use = "";
+            foreach (var item in studentinfo)
+            {
+
+                use = use + "\n" + item;
+            }
+            string word = use;
+
+            string path = @"C:\Users\RESIDENT FINTECH\Documents\Student Data\StudentInformation.txt";
+            FileStream fs = File.Open(path, FileMode.Create);
+
+
+
+            //to convert Studentdata.GetStudentdatas(); to string
+
+            byte[] buffer = Encoding.Default.GetBytes(word);
+            fs.Write(buffer, 0, buffer.Length);
+
+            fs.Position = 0;
+
+            //Filestream Read
+            byte[] filelist = new byte[buffer.Length];
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                filelist[i] = (byte)fs.ReadByte();
+            }
+            Console.WriteLine(Encoding.Default.GetString(filelist));
+            fs.Close();
+
+
+        }
+
+        //static void WriteToFile(List<Studentdata> students, string path)
+        //{
+        //    var filestream = new FileStream(path, FileMode.Open);
+        //    string usingToWrite = "";
+
+        //    foreach (var item in students)
+        //    {
+        //        usingToWrite = $"Name: {item.Name}, Age: {item.Age}, Class: {item.Class}";
+        //        byte[] toBytes = Encoding.Default.GetBytes(usingToWrite);
+        //        filestream.Write(toBytes, 0, toBytes.Length);
+        //    }
+        //    filestream.Close();
+        //}
+
+
+
+        //static void ReadFromFile(string path)
+        //{
+        //    var filestream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Read);
+
+        //    byte[] toBytes = new byte[1024];
+        //    var byteRead = filestream.Read(toBytes, 0, toBytes.Length);
+
+        //    var stringResult = Encoding.Default.GetString(toBytes, 0, byteRead);
+
+        //    Console.WriteLine(stringResult);
+        //    filestream.Close();
+        //}
+
+
+    }
+}
---
+++ Assignment_filestream/Studentdata.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_filestream
+{
+    internal class Studentdata
+    {
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Class { get; set; }
+        public decimal Grade { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}, Class: {Class}, Grade: {Grade}";
+        }
+
+        public static List<Studentdata> GetStudentdatas()
+        {
+            return new List<Studentdata>
+            {
+                new Studentdata { Name ="John", Age = 15, Class = "Year one", Grade = 5 },
+                new Studentdata { Name ="Peter", Age = 20, Class = "Year three", Grade = 7 },
+                new Studentdata { Name ="Cynthia", Age = 30, Class = "Year four", Grade = 4 },
+                new Studentdata { Name ="James", Age = 25, Class = "Year two", Grade = 2 },
+                new Studentdata { Name ="Ebuka", Age = 35, Class = "Year six", Grade = 3 },
+                new Studentdata { Name ="Bisi", Age = 10, Class = "Year five", Grade = 6 },
+
+
+
+            };
+        }
+    }
+}
